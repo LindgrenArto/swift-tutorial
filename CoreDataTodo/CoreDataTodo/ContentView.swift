8806424bd0d7f71e07d65f336a74543da55e84f2
Arto Lindgren <1701546@APO-IMAC-04.local>
2020-02-03 09:38:36 +0200

    CoreData almost done

--- CoreDataTodo/CoreDataTodo/ContentView.swift
+++ CoreDataTodo/CoreDataTodo/ContentView.swift
@@ -9,8 +9,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.managedObjectContext) var managedObjectContext
+    @FetchRequest(fetchRequest: TodoItem.getAllTodoItems()) var todoItems:FetchedResults<TodoItem>
+    
+    @State private var newTodoItem = ""
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView{
+            List{
+                Section(header: Text("What's next?")) {
+                    HStack{
+                        TextField("New Item", text: self.$newTodoItem)
+                        Button(action: {
+                            let todoItem = TodoItem(context: self.managedObjectContext)
+                            todoItem.title = self.newTodoItem
+                            todoItem.createdAt = Date()
+                            
+                            do{
+                                try self.managedObjectContext.save()
+                            }catch{
+                                print(error)
+                            }
+                            
+                            self.newTodoItem = ""
+                            
+                        }) {
+                            Image(systemName: "plus.circle.fill")
+                                .foregroundColor(.green)
+                                .imageScale(.large)
+                        }
+                        
+                    }.font(.headline)
+                }
+                Section(header: Text("To Do's")) {
+                    ForEach(self.todoItems) {todoitem in
+                        TodoItemView(title: todoitem.title!, createdAt: "\(todoitem.createdAt!)")
+                    }.onDelete {IndexSet in
+                        let deleteItem = self.todoItems[IndexSet.first!]
+                        
+                        self.managedObjectContext.delete(deleteItem)
+                        
+                        do{
+                            try self.managedObjectContext.save()
+                        }catch{
+                            print(error)
+                        }
+                    }
+                }
+            }
+            .navigationBarTitle("My List")
+            .navigationBarItems(trailing: EditButton())
+        }
     }
 }
 
