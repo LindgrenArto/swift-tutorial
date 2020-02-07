09f96512caa7b75c71dc592763e7eb32169214b7
Arto Lindgren <1701546@APO-IMAC-04.local>
2020-02-07 09:12:24 +0200

    Camera tutorial initial commit

---
+++ CameraTutorial/CameraTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CameraTutorial
+//
+//  Created by Arto Lindgren on 07/02/2020.
+//  Copyright © 2020 Arto Lindgren. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
